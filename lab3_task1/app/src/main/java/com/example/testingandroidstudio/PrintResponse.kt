236bdd443149750fc5fabc6548620024c92df272
Mikołaj Bigaj <mbigaj2@edu.cdv.pl>
2023-11-14 16:44:43 +0100

    Added some styling to layout of print

--- lab3_task1/app/src/main/java/com/example/testingandroidstudio/PrintResponse.kt
+++ lab3_task1/app/src/main/java/com/example/testingandroidstudio/PrintResponse.kt
@@ -1,7 +1,9 @@
 package com.example.testingandroidstudio
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +21,9 @@ class PrintResponse : AppCompatActivity() {
         val message_view: TextView = findViewById(R.id.regular_message)
         val ciphered_message_view: TextView = findViewById(R.id.ciphered_message_print)
         val message_switch: Switch = findViewById(R.id.message_switch)
+        val share_button: ImageView = findViewById(R.id.share_button)
+
+        var switch_state: Boolean = message_switch.isChecked
 
         val message = intent.getStringExtra(MESSAGE)
         val ciphered_message = intent.getStringExtra(CIPHERED_MESSAGE)
@@ -27,12 +32,25 @@ class PrintResponse : AppCompatActivity() {
         ciphered_message_view.text = ciphered_message
 
         message_switch.setOnClickListener() {
-            var switch_state: Boolean = message_switch.isChecked
+            switch_state = message_switch.isChecked
 
             if (switch_state)
                 message_view.visibility = View.INVISIBLE
             else
                 message_view.visibility = View.VISIBLE
+        }
+
+        share_button.setOnClickListener() {
+            val intent: Intent = Intent(Intent.ACTION_SEND)
+            switch_state = message_switch.isChecked
+
+            intent.setType("text/plain")
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            if (switch_state)
+                intent.putExtra(Intent.EXTRA_TEXT, ciphered_message)
+
+            startActivity(Intent.createChooser(intent, "Share using"))
         }
     }
 }
